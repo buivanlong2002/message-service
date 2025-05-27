982b3e5a59e3634683d2279d2d1fa76efa9f036c
buivanlong2002 <longbui20021008@gmail.com>
2025-05-28 00:06:10 +0700

    updatefile

--- src/main/java/com/example/message_service/controller/AuthController.java
+++ src/main/java/com/example/message_service/controller/AuthController.java
@@ -1,13 +1,16 @@
 package com.example.message_service.controller;
 
 
+import com.example.message_service.dto.ApiResponse;
 import com.example.message_service.dto.request.LoginRequest;
 import com.example.message_service.dto.request.RegisterRequest;
 import com.example.message_service.model.User;
 import com.example.message_service.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,25 +18,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/auth")
+@Validated
 public class AuthController {
 
     @Autowired
     private UserService userService;
 
-    // Đăng nhập
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<ApiResponse<String>> login(@Valid  @RequestBody LoginRequest loginRequest) {
         try {
-            String token = userService.loginUser(loginRequest.getUsername(), loginRequest.getPassword());
-            return ResponseEntity.ok(token);
+            ApiResponse<String> response = userService.loginUser(
+                    loginRequest.getUsername(),
+                    loginRequest.getPassword()
+            );
+            return ResponseEntity.ok(response);
+
         } catch (Exception ex) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Tài khoản hoặc mật khẩu không chính xác");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(ApiResponse.error("99", "Lỗi hệ thống"));
         }
     }
 
     // Đăng ký người dùng mới
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+    public ResponseEntity<?> register(@Valid  @RequestBody RegisterRequest request) {
         try {
             userService.registerUser(request);
             return ResponseEntity.status(HttpStatus.CREATED).body("Registration successful");
