835eb964b433ac25cddb34c9db6f2714f19b34cf
buivanlong2002 <longbui20021008@gmail.com>
2025-06-06 13:47:16 +0700

    addfile

--- src/main/java/com/example/message_service/dto/request/GetConversationByUserRequest.java
+++ src/main/java/com/example/message_service/dto/request/GetConversationByUserRequest.java
@@ -1,15 +1,13 @@
 package com.example.message_service.dto.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Setter
+@Getter
 public class GetConversationByUserRequest {
     private UUID userId;
 
-    public UUID getUserId() {
-        return userId;
-    }
-
-    public void setUserId(UUID userId) {
-        this.userId = userId;
-    }
 }
